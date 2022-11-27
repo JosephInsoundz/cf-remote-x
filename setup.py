1a383d221b16e2524e7fa06870401d285ace1afa
Joseph Matan <113097839+JosephInsoundz@users.noreply.github.com>
2022-11-27 15:07:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,14 +27,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cf-remote",
+    name="cf-remote-x",
     version=cf_remote_version,
     author="Northern.tech, Inc.",
     author_email="contact@northern.tech",
     description="Tooling to deploy CFEngine (and much more)",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cfengine/cf-remote",
+    url="https://github.com/cfengine/cf-remote-x",
     packages=setuptools.find_packages(),
     package_data={"cf_remote": ["VERSION"]},
     include_package_data=True,
